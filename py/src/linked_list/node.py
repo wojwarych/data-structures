02040ae2b0df9a4ed2743315f75001abf30ccccf
wojwarych <woj.warych@gmail.com>
2025-08-20 16:37:47 +0200

    added derived Node class for DoublyLinkedList

--- py/src/linked_list/node.py
+++ py/src/linked_list/node.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import TypeVar
 
 
@@ -6,15 +7,32 @@ T = TypeVar("T")
 
 class Node[T]:
     def __init__(self, value: T) -> None:
-        self.value = value
-        self.next = None
+        self._value = value
+        self._next: Node | None = None
 
     @property
     def value(self) -> T:
         return self._value
 
-    @value.setter
-    def value(self, value: T) -> None:
-        self._value = value
+    @property
+    def next(self) -> Node | None:
+        return self._next
+
+    @next.setter
+    def next(self, next_: Node | None) -> None:
+        self._next = next_
 
 
+
+class NextPrevNode[T](Node):
+    def __init__(self, value: T) -> None:
+        self._prev: NextPrevNode | None = None
+        super().__init__(value)
+
+    @property
+    def prev(self) -> NextPrevNode | None:
+        return self._prev
+
+    @prev.setter
+    def prev(self, prev_: NextPrevNode | None) -> None:
+        self._prev = prev_
