497ccf38542b9d106a7013c9dbec6b38cc676939
wojwarych <woj.warych@gmail.com>
2025-08-20 16:38:13 +0200

    move array queue to queues module

--- py/src/queues/array_queue.py
+++ py/src/queues/array_queue.py
@@ -2,18 +2,33 @@
 Implementing fixed-sized queue with "static" array
 Of course in Python lists are dynamic arrays but you might simulate the fixed size array.
 """
+from collections.abc import Iterator
 from typing import TypeVar
 
 T = TypeVar("T")
 
 
-class ArrayQueue:
-    def __init__(self, capacity: int = 5) -> None:
-        self._data = [None] * capacity
+class ArrayQueue[T]:
+    DEFAULT_CAP = 5
+
+    def __init__(self, capacity: int = DEFAULT_CAP) -> None:
+        self._data: list[T | None] = [None] * capacity
         self._capacity = capacity
         self._count = 0
         self._front = 0
         self._rear = 0
+
+    @property
+    def data(self) -> list[T | None]:
+        return self._data
+
+    @property
+    def capacity(self) -> int:
+        return self._capacity
+
+    @property
+    def count(self) -> int:
+        return self._count
 
     def enqueue(self, item: T) -> None:
         if self._count == self._capacity:
@@ -25,34 +40,25 @@ class ArrayQueue:
     def top(self) -> T:
         if self._count == 0:
             raise ValueError("Empty queue!")
-        return self._data[self._front % self._capacity]
+
+        ret = self._data[self._front % self._capacity]
+        assert ret is not None
+
+        return ret
 
     def pop(self) -> T:
         if self._count == 0:
             raise ValueError("Empty queue!")
+
         val = self._data[self._front % self._capacity]
+        assert val is not None
+
         self._data[self._front % self._capacity] = None
         self._front += 1
         self._count -= 1
         return val
 
-    def traverse(self) -> None:
+    def traverse(self) -> Iterator[T]:
         for item in range(self._front, self._rear):
             if (val := self._data[item % self._capacity]) is not None:
-                print(val)
-
-
-if __name__ == "__main__":
-    aq = ArrayQueue()
-    aq.enqueue(3)
-    aq.enqueue(4)
-    aq.enqueue(5)
-    aq.enqueue(6)
-    aq.enqueue(7)
-    aq.pop()
-    aq.pop()
-    aq.enqueue(8)
-    aq.enqueue(9)
-    aq.traverse()
-    aq.pop()
-    aq.traverse()
+                yield val
