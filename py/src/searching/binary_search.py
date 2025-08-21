fb0bf849b291c01336cff9eb5ef2738ad3fd0b93
wojwarych <woj.warych@gmail.com>
2025-08-21 11:29:55 +0200

    tests for searching methods

---
+++ py/src/searching/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(arr: list[int], value: int, start: int = 0, end: int | None = None) -> int | None:
+    if end is None:
+        end = len(arr) - 1
+    if start > end:
+        return None
+
+    mid = (start + end) // 2
+    if arr[mid] == value:
+        return arr[mid]
+    if arr[mid] > value:
+        return binary_search(arr, value, start, mid - 1)
+    return binary_search(arr, value, mid + 1, end)
---
+++ py/tests/unit/search/test_search_methods.py
@@ -0,0 +1,27 @@
+from collections.abc import Callable
+
+from src.searching.binary_search import binary_search
+from src.searching.linear_search import linear_search
+
+import pytest
+
+
+@pytest.fixture
+def list_input() -> list[int]:
+    return sorted([3, 99, 1, 11, 100, -5, -33])
+
+
+@pytest.mark.parametrize("method", [binary_search, linear_search])
+def test_binary_search_returns_correct_item(method: Callable, list_input: list[int]) -> None:
+    assert method(list_input, 99) == 99
+
+
+@pytest.mark.parametrize("method", [binary_search, linear_search])
+def test_binary_search_returns_correct_item_low_value(method: Callable, list_input: list[int]) -> None:
+    assert method(list_input, -5) == -5
+
+
+@pytest.mark.parametrize("method", [binary_search, linear_search])
+def test_binary_search_returns_no_item(method: Callable, list_input: list[int]) -> None:
+    assert not method(list_input, 900)
+
